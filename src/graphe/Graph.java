39e4f912d9ab5bec6a1c403c58ec0ffc761e47ce
Assia <assiabenmakhlouf06@gmail.com>
2025-04-08 10:36:12 +0200

    Fichier de base donnée

---
+++ src/graphe/Graph.java
@@ -0,0 +1,30 @@
+package graphe;
+import java.util.List;
+
+public class Graph {
+    /**
+     * Interface représentant un graphe.
+     *
+     * Elle n'impose qu'une unique méthode permettant de programmer un plus court chemin.
+     *
+     * @param <T> Identifiant des sommets. Le type doit disposer d'une méthode hashcode.
+     */
+    public interface graph<T> {
+        /**
+         * Un arc du graphe (désigné par sa valuation et le sommet destination).
+         *
+         * @param <T> Identifiant des sommets. Le type T doit être "hachable".
+         */
+        public record Arc<T>(int val, T dst) {
+        }
+
+        /**
+         * Donne la liste des arcs sortants d'un sommet.
+         *
+         * @param s Le sommet.
+         * @return La liste des arcs sortants de {@code s}.
+         */
+        List<Arc<T>> getSucc(T s);
+    }
+
+}
