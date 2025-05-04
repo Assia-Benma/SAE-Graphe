f3ceae408c7639f9269345a1a9e6d255cf0e10d3
Assia <assiabenmakhlouf06@gmail.com>
2025-05-04 19:26:56 +0200

    coquille corriger GraphImplTest.java

--- src/graphe/GraphImplTest.java
+++ src/graphe/GraphImplTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class GraphImplTest {
+class GraphImpITest {
 
     private GraphImpl<String> grapheString;
     private GraphImpl<Integer> grapheInteger;
