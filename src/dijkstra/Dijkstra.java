08ba015496f1a70d7d7e60a279aad7967a3ad8e0
krabi <131712799+Krabibi@users.noreply.github.com>
2025-05-09 16:46:32 +0200

    Add files via upload

---
+++ src/dijkstra/Dijkstra.java
@@ -0,0 +1,14 @@
+package dijkstra;
+
+import graphe.Graph;
+import graphe.ShortestPath;
+
+public class Dijkstra<T> implements ShortestPath<T> {
+
+	@Override
+	public Distances<T> compute(Graph.graph<T> g, T src, Animator<T> animator) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
